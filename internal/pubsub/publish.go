e397173190927059f8d6fa03fb02ed652a917f91
Joenatan <Joenatan506@gmail.com>
2026-05-07 16:11:55 +0700

    feat: add transient queue declaration and client RabbitMQ connection

--- internal/pubsub/publish.go
+++ internal/pubsub/publish.go
@@ -8,14 +8,14 @@ import (
 )
 
 func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
-	jsonBytes, err := json.Marshal(val)
+	dat, err := json.Marshal(val)
 	if err != nil {
 		return err
 	}
 
 	msg := amqp.Publishing{
 		ContentType: "application/json",
-		Body:        jsonBytes,
+		Body:        dat,
 	}
 
 	err = ch.PublishWithContext(context.Background(), exchange, key, false, false, msg)
